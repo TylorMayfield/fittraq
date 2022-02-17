67b6eb7434073b2f63a8d9617289c5107b26b43f
Tylor Mayfield <tmayfield@dealerpolicy.com>
2022-02-17 13:57:18 -0500

    404

---
+++ src/pages/_fallback.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { url } from "@roxi/routify";
+  import { Button } from "carbon-components-svelte";
+</script>
+
+<div class="e404">
+  <div class="huge">Nope</div>
+  <div class="big">
+    <br />
+    <br />
+    <a href={$url("../")}><Button kind="secondary">Go back</Button></a>
+  </div>
+</div>
+
+<style>
+  .huge {
+    font-size: xx-large;
+  }
+  .e404 {
+    position: absolute;
+    left: 50%;
+    top: 40%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+</style>
